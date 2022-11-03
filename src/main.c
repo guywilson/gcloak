339ee51cf24d64d2a45131c5f23d55ace7795bef
Guy Wilson <wilson.guy@gmail.com>
2022-11-03 17:49:25 +0000

    Image box working

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@ static void activate(GtkApplication * app, gpointer user_data)
     GtkWidget *         image;
     GtkWidget *         capacityLabel;
     GtkWidget *         imageBox;
+    GtkWidget *         imageFrame;
     GdkPixbuf *         pixbuf;
 
     mainWindow = gtk_application_window_new(app);
@@ -23,12 +24,15 @@ static void activate(GtkApplication * app, gpointer user_data)
 
     imageBox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 
+    imageFrame = gtk_frame_new(NULL);
+
     pixbuf = gdk_pixbuf_new_from_file_at_size("/Users/guy/development/cloak/test/album.bmp", 500, 500, NULL);
     image = gtk_image_new_from_pixbuf(pixbuf);
 
     gtk_widget_set_size_request(image, 400, 400);
 
     capacityLabel = gtk_label_new_with_mnemonic("_Capacity: ");
+    gtk_widget_set_halign(capacityLabel, GTK_ALIGN_START);
 
     gtk_box_append(GTK_BOX(imageBox), image);
     gtk_box_append(GTK_BOX(imageBox), capacityLabel);
@@ -40,7 +44,14 @@ static void activate(GtkApplication * app, gpointer user_data)
     gtk_widget_set_margin_start(imageBox, 10);
     gtk_widget_set_margin_end(imageBox, 10);
 
-    gtk_window_set_child(GTK_WINDOW(mainWindow), imageBox);
+    gtk_frame_set_child(GTK_FRAME(imageFrame), imageBox);
+
+    gtk_widget_set_margin_top(imageFrame, 10);
+    gtk_widget_set_margin_bottom(imageFrame, 10);
+    gtk_widget_set_margin_start(imageFrame, 10);
+    gtk_widget_set_margin_end(imageFrame, 10);
+
+    gtk_window_set_child(GTK_WINDOW(mainWindow), imageFrame);
 
     gtk_widget_set_halign(mainWindow, GTK_ALIGN_START);
     gtk_widget_set_valign(mainWindow, GTK_ALIGN_CENTER);
